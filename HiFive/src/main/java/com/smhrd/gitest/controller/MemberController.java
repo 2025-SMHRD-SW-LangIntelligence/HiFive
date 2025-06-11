6f814545935aa20e7c483451b0f0665f3667a362
DOrigin1202 <jde577776@gmail.com>
2025-06-11 12:50:08 +0900

    dd

--- HiFive/src/main/java/com/smhrd/gitest/controller/MemberController.java
+++ HiFive/src/main/java/com/smhrd/gitest/controller/MemberController.java
@@ -7,7 +7,7 @@ public class MemberController {
 		System.out.println("정동인");
 		System.out.println("정광원");
 		System.out.println("고서연");
-		System.out.println("김동진");
+		System.out.println("동진것 삭제하고 동인것 작성");
 	}
 
 }
