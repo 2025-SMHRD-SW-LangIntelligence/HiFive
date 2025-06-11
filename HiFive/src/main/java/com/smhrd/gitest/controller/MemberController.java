e72e3057cb8e8aa20f5bb565fc76efc616c30060
smhrd <smhrd@192.168.219.52>
2025-06-11 14:58:15 +0900

    0611조장 김동진

--- HiFive/src/main/java/com/smhrd/gitest/controller/MemberController.java
+++ HiFive/src/main/java/com/smhrd/gitest/controller/MemberController.java
@@ -3,12 +3,8 @@ package com.smhrd.gitest.controller;
 public class MemberController {
 		
 	public static void main(String[] args) {
+		
 		System.out.println("김동진");
-		System.out.println("정동인");
-		System.out.println("정광원");
-		System.out.println("고서연");
-		System.out.println("임태영");
-		System.out.println("dj branch");
-		System.out.println();
+
 	}
 } 
