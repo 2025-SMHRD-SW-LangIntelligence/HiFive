dea270d1fafed7a72f06a19983e5c1b0d5e541a6
smhrd <smhrd@192.168.219.47>
2025-06-12 12:18:30 +0900

    필요없는 정보 제거 - 광원

--- HiFive/src/main/java/com/smhrd/gitest/controller/MemberController.java
+++ HiFive/src/main/java/com/smhrd/gitest/controller/MemberController.java
@@ -5,15 +5,6 @@ public class MemberController {
 	public static void main(String[] args) {
 
 		
-		System.out.println("하이파이브");
-		System.out.println("브렌치 테에에스트");
-		System.out.println("dj branch");
-
-		System.out.println("누가 다 밀어버렸어");
-		System.out.println("김동진");
 		
-		String merge = "가나다";
-		System.out.println(merge);
-
 	}
 } 
