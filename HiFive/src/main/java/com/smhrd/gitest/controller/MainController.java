2630a93d6c1523a57cf296afb01f23a612bc069a
smhrd <smhrd@192.168.219.47>
2025-06-12 16:15:31 +0900

    데이터베이스pw 추가

--- HiFive/src/main/java/com/smhrd/gitest/controller/MainController.java
+++ HiFive/src/main/java/com/smhrd/gitest/controller/MainController.java
@@ -1,5 +1,6 @@
 package com.smhrd.gitest.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +15,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 
 public class MainController {
+	
+	
+	
 @GetMapping("/login")
 public String login() {
 	return "login";
