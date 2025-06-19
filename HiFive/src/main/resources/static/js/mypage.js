9454275602fc1a67009690f5a39170e545191afe
smhrd <smhrd@192.168.219.47>
2025-06-19 15:04:31 +0900

    0619/광원/prototype 데이터인 마이페이지 리뷰 찜 갯수 수정

--- HiFive/src/main/resources/static/js/mypage.js
+++ HiFive/src/main/resources/static/js/mypage.js
@@ -1,13 +1,13 @@
 // 뒤로가기 버튼 기능
-function goBack() {
-  window.location.href = "result.html";
-}
+// function goBack() {
+  // window.location.href = "result.html";
+//}
 
 // 로컬에서 리뷰 수, 찜 수 불러오기
-document.addEventListener("DOMContentLoaded", () => {
-  const reviewCount = localStorage.getItem("reviewCount") || 0;
-  const wishlistCount = localStorage.getItem("wishlistCount") || 0;
+// document.addEventListener("DOMContentLoaded", () => {
+  // const reviewCount = localStorage.getItem("reviewCount") || 0;
+  // const wishlistCount = localStorage.getItem("wishlistCount") || 0;
 
-  document.getElementById("review-count").textContent = reviewCount;
-  document.getElementById("like-count").textContent = wishlistCount;
-});
+  // document.getElementById("review-count").textContent = reviewCount;
+  // document.getElementById("like-count").textContent = wishlistCount;
+//});
