f526f75a5e9c899a5d8374dbcaf434d1ee68ab87
smhrd <smhrd@192.168.219.47>
2025-06-23 10:12:27 +0900

    0623/광원/리뷰수정에러 해결 및 찜목록취소 에러해결

--- HiFive/src/main/java/com/smhrd/gitest/service/MemberWishlistService.java
+++ HiFive/src/main/java/com/smhrd/gitest/service/MemberWishlistService.java
@@ -2,8 +2,13 @@ package com.smhrd.gitest.service;
 
 import com.smhrd.gitest.entity.MemberWishlistEntity;
 import com.smhrd.gitest.repository.MemberWishlistRepository;
+
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 
 @Service
@@ -13,6 +18,7 @@ public class MemberWishlistService {
     private MemberWishlistRepository wishlistRepository;
 
     // 찜 추가
+    @Transactional
     public String addWishlist(String email, Long store_id) {
         if (wishlistRepository.existsByEmailAndStoreId(email, store_id)) {
             return "already"; // 이미 찜함
@@ -25,15 +31,18 @@ public class MemberWishlistService {
     }
 
     // 찜 취소
+    @Transactional //DB의 데이터를 삭제하는 트랜잭션 처리
     public void removeWishlist(String email, Long store_id) {
         wishlistRepository.deleteByEmailAndStoreId(email, store_id);
     }
 
     // 내 찜한 술집 리스트
+    @Transactional(readOnly = true)
     public List<MemberWishlistEntity> getMyWishlist(String email) {
         return wishlistRepository.findAllByEmail(email);
     }
  // ★ 찜 여부 확인 메소드 추가
+    @Transactional(readOnly = true)
     public boolean existsByEmailAndStoreId(String email, Long storeId) {
         return wishlistRepository.existsByEmailAndStoreId(email, storeId);
     }
